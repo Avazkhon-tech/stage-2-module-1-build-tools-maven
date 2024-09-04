a4003f132722f5ef2078eb83233cc2c47fdb489c
Avazkhon <avazxonnazirov334@gmail.com>
2024-09-04 12:13:25 +0500

    "first try"

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,19 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
 import java.util.List;
+
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+       for (String arg : args) {
+           boolean positiveNumber = StringUtils.isPositiveNumber(arg);
+           if (!positiveNumber) {
+               return false;
+           }
+       }
+
+       return true;
     }
 }
